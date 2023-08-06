a228ba8160995c54aec11973645eb570dbacc07e
Kamaliki Clive <47914903+kamaliki@users.noreply.github.com>
2023-08-06 11:50:02 +0300

    Create rotate.js
    rotate an array

---
+++ rotate array/rotate.js
@@ -0,0 +1,30 @@
+function rotate(nums, k) {
+  if (k > nums.length) {
+    k = k % nums.length;
+  }
+  
+  const result = new Array(nums.length);
+  
+  // Rotate the last k elements and place them at the beginning of the result array
+  for (let i = 0; i < k; i++) {
+    result[i] = nums[nums.length - k + i];
+  }
+  
+  let j = 0;
+  
+  // Rotate the remaining elements and place them after the k rotated elements in the result array
+  for (let i = k; i < nums.length; i++) {
+    result[i] = nums[j];
+    j++;
+  }
+  
+  // Copy the elements from the result array back to the original nums array
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = result[i];
+  }
+
+  console.log(nums);
+}
+
+//usage
+rotate([1,2,3,4,5,6], 7);
